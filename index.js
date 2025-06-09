93ad9f989b7dd8fbd51a4a8065b24cfa41331f81
abhijith-appz <akm024134@gmail.com>
2025-06-09 09:10:34 +0530

    database connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require("express");
-const mongoose=require("mongoose")
+const mongoose=require("mongoose");
 const PORT=8000;
 
 //init app
 const app=express();
 const connectionUrl="mongodb://localhost:27017/todoDb";
+
+mongoose.connect(connectionUrl).then(()=> console.log("Database connected succesfully")).catch((error)=>console.log(error.message));
 
 //view engine
 app.set("view engine","ejs");
