404365f51f3f847473ea396fc2fe6a39d47874f1
abhijith-appz <akm024134@gmail.com>
2025-06-10 12:35:20 +0530

    adding the add todo page

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const PORT=8000;
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 8000;
 
 //init app
-const app=express();
-const connectionUrl="mongodb://localhost:27017/todoDb";
+const app = express();
+const connectionUrl = "mongodb://localhost:27017/todoDb";
 
-mongoose.connect(connectionUrl).then(()=> console.log("Database connected succesfully")).catch((error)=>console.log(error.message));
+mongoose.connect(connectionUrl).then(() => console.log("Database connected succesfully")).catch((error) => console.log(error.message));
 
 //view engine
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get("/", (req,res,next)=> {
-    try{
+app.get("/", (req, res, next) => {
+    try {
         res.render("index");
-    }catch(error){
-        res.status(500).json({message:error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+app.get("/add-todo", (req, res, next) => {
+    try {
+        res.render("newTodo");
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
 //listen server
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
 
 });
