47c10909474f62d2e38dc953c0b2ac9886de4215
abhijith-appz <akm024134@gmail.com>
2025-06-14 22:48:38 +0530

    add-todo functionality using todo model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const bodyParser = require("body-parser");
 const PORT = 8000;
 
 //init app
@@ -9,40 +10,54 @@ const connectionUrl = "mongodb://localhost:27017/todoDb";
 
 mongoose.connect(connectionUrl).then(() => console.log("Database connected succesfully")).catch((error) => console.log(error.message));
 
-const todoSchema=mongoose.Schema({title:{type :String,required:true},desc:String},{timestamps:true});
+const todoSchema = mongoose.Schema({ title: { type: String, required: true }, desc: String }, { timestamps: true });
 
-const Todo=mongoose.model("todo",todoSchema);
+const Todo = mongoose.model("todo", todoSchema);
 
 //view engine
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res, next) => {
     try {
-        res.render("index",{title: "List todo"});
+        res.render("index", { title: "List todo" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 app.get("/add-todo", (req, res, next) => {
     try {
-        res.render("newTodo",{title: "Add todo"});
+        res.render("newTodo", { title: "Add todo" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 app.get("/update-todo", (req, res, next) => {
     try {
-        res.render("updateTodo",{title: "Update todo"});
+        res.render("updateTodo", { title: "Update todo" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
-app.get("/delete-todo",(req,res,next)=>{
-    try{
-        res.render("deleteTodo",{title: "Delete todo"})
+app.get("/delete-todo", (req, res, next) => {
+    try {
+        res.render("deleteTodo", { title: "Delete todo" });
 
-    }catch(error){
-        res.status(500).json({message:error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+app.post("/add-todo", async(req, res, next) => {
+    try{
+        const { title, desc } = req.body;
+        if(!title){
+            return res.status(400).json({message :"title is required"});
+        }
+        const newTodo = new Todo({ title, desc });
+        await newTodo.save();
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
 });
 
