d9ccd8aa7b57457c0037749eec12a5ae3e5a79ce
abhijith-appz <akm024134@gmail.com>
2025-07-11 22:33:33 +0530

    seperate app configuration and server listening

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const app=require("./app");
 const PORT = 8000;
-const connectMongodb=require("./init/mongodb")
-const todoroute=require("./routes/todo")
-//init app
-const app = express();
-//mongodb connection
-connectMongodb();
-//view engine
-
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/",todoroute);
 
 
 //listen server
